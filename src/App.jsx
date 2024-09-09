acfabe561a625e7ed5c015b13de4105fef9e20d0
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-09-09 16:26:47 +0530

    the begining design created and created 3 choise buttons , and functioned fo changing the city name with use of useeffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [city, setCity] = useState("Kochi");
+
+  function changeCity(selectedCity){
+    setCity(selectedCity.target.dataset.city)
+    console.log(city);
+  }
+
 
   return (
     <>
-     
+     <h1>Temparature at {city} is 27&deg;C</h1>
+     <div className='suggestions'>
+      <button onClick={() => changeCity("Kochi")}>Kochi</button>
+      <button onClick={() => changeCity("Tiruvanandrum")}>Tiruvanandrum</button>
+      <button onClick={() => changeCity("Kottayam")}>Kottayam</button>
+     </div>
     </>
   )
 }
